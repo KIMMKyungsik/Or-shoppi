fd588523eb7ebf51c91b75070cbb646d4b091ba5
KIMMKyungsik <ksiki1@naver.com>
2022-07-31 17:16:21 +0900

    홈 화면 JSON asset 추가 및 불러오기 구현

--- app/src/main/java/com/shoppi/app/HomeFragment.kt
+++ app/src/main/java/com/shoppi/app/HomeFragment.kt
@@ -1,12 +1,14 @@
 package com.shoppi.app
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import org.json.JSONObject
 
 class HomeFragment : Fragment() {
 
@@ -15,16 +17,32 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home,container,false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-       val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
+        val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
         button.setOnClickListener {
             findNavController().navigate(R.id.action_home_to_product_detail)
         }
+
+        val assetLoader = AssetLoader()
+        val homeData = assetLoader.getJsonString(requireContext(),"home.json")
+        Log.d("homeData",homeData ?: "")
+
+
+        if(homeData.isNullOrEmpty()){
+        val jsonObject = JSONObject(homeData)
+            val title = jsonObject.getJSONObject("title")
+            val text = title.getString("text")
+            val iconUrl= title.getString("icon_url")
+
+
+        }
+
+
     }
 
 }
